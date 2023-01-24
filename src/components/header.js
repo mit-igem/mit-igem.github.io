8b0f7a7bd68972ecfd4a6a64306ae158bafdfb29
Matthew Feng <mattfeng@mit.edu>
2023-01-24 12:43:27 -0500

    ginkgo

--- src/components/header.js
+++ src/components/header.js
@@ -19,7 +19,7 @@ const Header = () => {
 
       <nav className={styles.nav}>
         <a href="/#about">About</a>
-        <Link href="/ginkgo">Ginkgo RSVP form</Link>
+        <Link href="/ginkgo">Ginkgo 01/24 talk</Link>
         <a href="/#igem2023">iGEM 2023</a>
         <a href="/#events">Events</a>
         <a href="/#sponsors">Sponsors</a>
